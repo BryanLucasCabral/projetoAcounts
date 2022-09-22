ae27f6ca73bd94e1a3c31f941d7fd89ff9b4e2ae
Bryan Lucas Cabral Do Nascimento <bryan.brasil.bl@gmail.com>
2022-09-22 19:45:48 -0300

    lista de escolhas

--- acounts/index.js
+++ acounts/index.js
@@ -1,1 +1,16 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
+
+operation()
+function operation () {
+    inquirer.prompt([{
+        type:'list',
+        name:'action',
+        Message:'O que você deseja fazer?',
+        choices:['Criar conta','depositar','consultar saldo','sacar','sair']
+    }]).then((answers) => {
+        const action = answers('action')
+        console.log(action)
+    }).catch((err) =>console.log(err))
+
+}
